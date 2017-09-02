08640dceb6160ea81c4f75a748e9a46c4a6cb144
Disako <p.b.davies1185@gmail.com>
2017-09-02 18:27:25 +0100

    Adding missing files

---
+++ Assets/Deck/PlayerLifeBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLifeBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public GameObject Deck;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Deck != null)
+        {
+            var deck = Deck.GetComponent<DeckBehaviour>();
+
+            var text = gameObject.GetComponent<UnityEngine.UI.Text>();
+            
+            if(text != null)
+                text.text = "Life: " + deck.State.Life;
+        }
+	}
+}
---
+++ Assets/Deck/PlayerState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class PlayerState
+{
+    public int CardsInDeck = 60;
+    public Team Owner;
+    public List<CardState> KnownCards;
+    public int Life = 10;
+
+    public PlayerState Clone()
+    {
+        var newState = new PlayerState()
+        {
+            CardsInDeck = CardsInDeck,
+            Owner = Owner,
+            KnownCards = new List<CardState>(),
+            Life = Life
+        };
+        foreach (var knownCard in KnownCards)
+        {
+            newState.KnownCards.Add(knownCard.Clone());
+        }
+        return newState;
+    }
+}
