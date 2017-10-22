fd0fc65c8ec2a5b6a26ae78ebb282c72ec8dc048
Disako <p.b.davies1185@gmail.com>
2017-10-22 21:32:45 +0100

    Adding an indicator to show which player each card on the board belongs to

---
+++ Assets/Card/OwnerIndicatorBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnerIndicatorBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var cardState = GetComponentInParent<CardBehaviour>().State;
+
+        var renderer = gameObject.GetComponent<MeshRenderer>();
+        if (cardState.CurrentZone == Zone.InPlay)
+        {
+            renderer.enabled = true;
+            renderer.material.color = cardState.Owner == Team.Opponent ? Color.red : Color.blue;
+        }
+        else
+        {
+            renderer.enabled = false;
+        }
+	}
+}
